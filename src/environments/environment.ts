9a793b51d227dadd6cff46fbd3a3da9bf15428ea
thomas maestas <thomasm1.maestas@gmail.com>
2020-02-17 14:44:03 -0500

    news API

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '06voWGzUHt0AJNvF2CeIqGezsRBQTZd5',
+  apiUrl: 'https://api.nytimes.com/svc'
 };
 
 /*
