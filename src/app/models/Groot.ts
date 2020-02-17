52b123d27054967d2b27315d37bbf8fb3ffa33cf
thomas maestas <thomasm1.maestas@gmail.com>
2020-02-16 23:42:26 -0500

    data pointing to ec2 servers

--- src/app/models/Groot.ts
+++ src/app/models/Groot.ts
@@ -1,11 +1,15 @@
 export class Groot {
 
     id :number;
+    author:string;
+    title:string;
     name :string;
     type :string;
 
-    constructor(id :number, name :string, type :string) {
+    constructor(id :number, author :string, title :string, name :string, type :string) {
         this.id = id;
+        this.author = author;
+        this.title = title;
         this.name = name;
         this.type = type;
     }
