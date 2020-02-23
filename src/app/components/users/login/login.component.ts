cf113a9a7e3e4e0d56735b8484ba3c3ca6d4710d
thomas maestas <thomasm1.maestas@gmail.com>
2020-02-23 01:04:28 -0500

    login/register, authentication service

--- src/app/components/users/login/login.component.ts
+++ src/app/components/users/login/login.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginModel } from '../../../models/login';
-import { FormGroup, FormBuilder, Validators,  } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormGroup, FormBuilder, Validators, } from '@angular/forms';
+
+import { first } from 'rxjs/operators';
+import { AlertService, AuthenticationService } from '../../../services';
 
 @Component({
   selector: 'app-login',
@@ -9,33 +13,67 @@ import { FormGroup, FormBuilder, Validators,  } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
- 
+
   title: string = "";
   user: LoginModel = new LoginModel();
   loginForm: FormGroup;
   hide = true;
+  loading = false;
+  submitted = false;
+  returnUrl: string;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private authenticationService: AuthenticationService,
+    private alertService: AlertService
+  ) {
+    // redirect to home if already logged in
+    if (this.authenticationService.currentUserValue) {
+      this.router.navigate(['/']);
+    }
+  }
 
 
   ngOnInit() {
-   this.title = "LOGIN"; 
+    this.title = "LOGIN";
 
-    this.loginForm = this.formBuilder.group({ 
+    this.loginForm = this.formBuilder.group({
       'email': [this.user.email, [
-       Validators.required,
-       Validators.email 
+        Validators.required,
+        Validators.email
       ]],
       'password': [this.user.password, [
-     Validators.required,
-       Validators.minLength(4),
-       Validators.maxLength(30) 
-        
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(30)
+
       ]]
     });
+    // get return url from route parameters or default to '/'
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
   }
+  // convenience getter for easy access to form fields
+  get f() { return this.loginForm.controls; }
 
   onLoginSubmit() {
-    alert(  this.user.email + ' ' + this.user.password);
+    alert(this.user.email + ' ' + this.user.password);
+    this.submitted = true;
+
+    // reset alerts on submit
+    this.alertService.clear();
+    this.loading = true;
+    this.authenticationService.login(this.f.username.value, this.f.password.value)
+      .pipe(first())
+      .subscribe(
+        data => {
+          this.router.navigate([this.returnUrl]);
+        },
+        error => {
+          this.alertService.error(error);
+          this.loading = false;
+        });
   }
+
 }
