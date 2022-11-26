465ad2816091c5cbb32d03f90e8f19688a52f6ea
Thomas Maestas <thomasm1.maestas@gmail.com>
2022-11-26 15:37:47 -0500

    observables syntax clean-up

--- src/app/components/crypto/nfts.component.ts
+++ src/app/components/crypto/nfts.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Subject, Subscription } from 'rxjs';
 import { NftsService } from '../../services/nfts.service';
 
 @Component({
@@ -10,17 +10,22 @@ import { NftsService } from '../../services/nfts.service';
 })
 
 export class NftsComponent implements OnInit, OnDestroy { 
-  nfts: any[] = []; 
+  nfts: any  ; 
+  nftData: any;
+  tokens: any = [];
+  nftsUpdated = new Subject<any[]>();
   private nftSubscription: Subscription = new Subscription; 
-  private blogSubscription: Subscription = new Subscription;
+ 
 
   constructor(
     private nftsService: NftsService
   ) {
-    this.nfts = this.nftsService.collectNfts(); 
+    this.nfts = this.loadNfts();
   }
 
   ngOnInit(): void { 
+
+
     this.nftSubscription = this.nftsService.nftsUpdated.subscribe(() => {
       this.nfts = this.nftsService.collectNfts(); 
     });
@@ -30,6 +35,22 @@ export class NftsComponent implements OnInit, OnDestroy {
     });
  
   }
+  loadNfts() {
+    this.nfts = this.nftsService.collectNfts()  
+    .subscribe((data: any) => {
+     if (data != undefined) {
+       this.nftData = data;
+       console.log("this.nftData")
+       console.log(this.nftData);
+
+       this.tokens = data.tokens; 
+
+       this.nfts = data.nfts;
+       console.log(this.nfts);
+       console.log(this.nfts[0])
+       this.nftsUpdated.next([...this.nfts]);
+     }
+   }); }
 
   ngOnDestroy(): void {
     this.nftSubscription.unsubscribe();
--- src/app/components/layout/about/about.component.ts
+++ src/app/components/layout/about/about.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
-import { Observable } from 'rxjs';
+import {  Observable, map } from 'rxjs';
 import { Greeting } from '../../../models/Greeting';
+import { createHttpObservable } from 'src/app/utility/observable';
  
 @Component({
   selector: 'app-about',
@@ -9,26 +10,28 @@ import { Greeting } from '../../../models/Greeting';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-
+  http$!: Observable<any[]>;
+  data$!: Observable<any[]>; 
+  web3$!: Observable<any[]>;
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
 
-    this.greeting();
-
-  }
-   info:string = "";
-  greeting() {
-    let result: Promise<any> = this.http.get<any>("https://z3noflrq9b.execute-api.us-east-1.amazonaws.com/dev/posts").toPromise();
-
-    result.then(
-      (res) =>{
-      
-        console.log(res);
-      }
-    ); 
-
-  } 
+   this.http$ = createHttpObservable("https://z3noflrq9b.execute-api.us-east-1.amazonaws.com/dev/posts");
+    this.http$.subscribe(console.log);
+    this.web3$ = this.http$
+    .pipe(
+      map(res => Object.values(res)
+      .filter((res: any) => res.cat3 === "Musing Blockchain") 
+      ));
+}
+  vision:string =``;
+  about:string = ``;
+   mission:string = `This website is designed for the web3 experimentation and development, 
+   sponsored by the Friends of Groot Society, intended for fellow Groot fans in an web3 development environment. 
+   <br />Find news, events, photos, and other Groot-related features in the groot society database. 
+   Please feel free to Contact the Society for News and Updates!`;
+   data: any = [];
  
 
 
